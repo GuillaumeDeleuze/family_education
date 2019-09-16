be40db8c8c9ce6b1d63fd1adb415332bea07e2a7
Guillaume <deleuzegui@hotmail.com>
2019-09-16 13:06:41 +0200

    Sarah modif

--- app/javascript/plugins/suggestion.js
+++ app/javascript/plugins/suggestion.js
@@ -3,7 +3,7 @@ let missionName = document.getElementById('mission_name');
 
 
 function addSuggestion(e) {
-  missionName.value = e.target.innerHTML
+  missionName.value = e.target.innerHTML.replace(/^\s+/g, '')
 };
 
 suggestions.forEach(function(suggestion){
